a1be3c0fb8a73a468630f659367dec0f41058456
Maria Henylie Gyanna <gyannahenylie@gmail.com>
2024-02-02 21:09:51 +0800

    for js and jpg import

---
+++ HTMLExercises/greet.js
@@ -0,0 +1,7 @@
+function greetUser() {
+	var name = prompt("What is your name?");
+	if(name) {
+		alert("Hello, " + name + "! Join us in Unvailing Fukuoka's Hidden Chrams!");
+            }
+        }
+        greetUser();
